62df8267d1d139134948e56ab7c81e10c86de4e5
pitbrest <sasha.petrov@tut.by>
2022-01-24 13:51:13 +0300

    feat: add pictures changing when current season button activated

---
+++ portfolio-seasons.js
@@ -0,0 +1,43 @@
+// Active vision of portfolio buttons
+
+const portfolioButtons = document.querySelectorAll('.portfolio-buttons__item')
+const portfolioImages = document.querySelectorAll('.portfolio-imagies')
+
+portfolioButtons.forEach((button) => {
+
+	button.addEventListener('click', (event) => {
+		event.preventDefault()
+
+		portfolioButtons.forEach(item => item.classList.remove('current'))
+		toggleSeasonsPhoto()
+
+		portfolioImages.forEach(item => item.classList.remove('active'))
+
+		if (button.classList.contains('winter')) {
+			document.getElementById('winter').classList.toggle('active')
+		}
+		if (button.classList.contains('summer')) {
+			document.getElementById('summer').classList.toggle('active')
+		}
+		if (button.classList.contains('autumn')) {
+			document.getElementById('autumn').classList.toggle('active')
+		}
+		if (button.classList.contains('spring')) {
+			document.getElementById('spring').classList.toggle('active')
+		}
+
+	})
+
+	const toggleSeasonsPhoto = () => {
+		button.classList.toggle('current')
+	}
+})
+
+
+// Change images for current active season portfolio button
+
+
+
+
+
+
