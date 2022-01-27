788144c4d9daedefc422768269b9c99509b7cfa7
pitbrest <sasha.petrov@tut.by>
2022-01-27 16:45:57 +0300

    some commit

--- preload-images.js
+++ preload-images.js
@@ -3,11 +3,11 @@ const seasons = ['winter', 'spring', 'summer', 'autumn'];
 
 function preloadImages(arr) {
 
-	for (let a = 0; a<arguments.length; a++) {
+	for (let a = 0; a < arguments.length; a++) {
 
 		for (let i = 1; i <= 6; i++) {
 			const img = new Image();
-			img.src = `/assets/jpg/${arguments[a]}/${i}.jpg`;
+			img.src = `assets/jpg/${arguments[a]}/${i}.jpg`;
 		}
 	}
 }
